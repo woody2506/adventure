2fb7dc23fd7b2c24c7efc780c412cebf6906c014
starweb2026 <starweb2026@outlook.com>
2026-07-01 19:38:26 +0800

    new version

--- main.py
+++ main.py
@@ -5791,7 +5791,8 @@ def gamestart():
                             print('Sorry, I do not understand that word.')
                     else:
                         print('You have alrady dig the grave!')
-        
+        else:
+            print('Unknown command.')
         if game_over == True:
             print("=== END ===")
             print("Type 'menu' to return main menu")
